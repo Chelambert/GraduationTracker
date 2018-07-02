d85cf68d18d981ed493f6f5d8427737f8aad2752
Chelsea Pontbriand <Chelsea.Pontbriand@gmail.com>
2018-07-02 14:20:12 -0400

    Fixed positioning in meeting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,10 @@
-const something = 'something';
-console.log(something);
+// var header = document.getElementById("main-nav");
+// var icons = header.getElementsByClassName("nav-item");
 
-const person = {
-    firstName: 'jerm',
-    lastName: 'law',
-    age: 33
-};
-
-let thing;
-
-const {
-    firstName,
-    lastName,
-    age
-} = person;
-console.log(`${firstName} ${lastName} ${age}`);
-
-const { x, y, ...z } = { x:1, y: 2, a: 3, b: 4};
-
+// for(var i = 0; i < icons.length; i++){
+//     btns[i].addEventListener("click", function() {
+//         var current = document.getElementByClassName("nav-item--active");
+//         current[0].className = current[0].className.replace(" nav-item--active", "");
+//         this.className += " nav-item--active";
+//     });
+// }
